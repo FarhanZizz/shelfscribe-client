12f338125494abca39396a0d0d169d13824cc3b7
FarhanZizz <iamfarhanaziz@gmail.com>
2023-07-15 22:24:08 +0600

    footer added

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "./Navbar";
+import Footer from "../components/Footer";
 const MainLayout = () => {
   return (
     <div className="pt-6 md:px-10">
       <Navbar />
       <div className="px-4">
         <Outlet />
+        <Footer />
       </div>
     </div>
   );
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { Link } from "react-router-dom";
 import { BsGoogle } from "react-icons/bs";
 import img from "../assets/book.png";
